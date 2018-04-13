d7911c4fbdaaec449fb381c890eaf14dd7376fed
uforgot <uforgot@designfever.com>
2018-04-13 18:52:13 +0900

    top include 처리 및 링크 수정

---
+++ markup_template/assets/js/include.js
@@ -0,0 +1,13 @@
+/**
+ * -----------------------------------------------------
+ * Created by uforgot on 2018. 4. 13.
+ * work.designfever.com
+ * -----------------------------------------------------
+ */
+
+
+$(window).on('load',function(){
+    $('#top').load('include/top.html .top');
+    $('#sub-men-2').load('include/top.html .sub-menu-2');
+    $('#sub-menu-7').load('include/top.html .sub-menu-7');
+});
