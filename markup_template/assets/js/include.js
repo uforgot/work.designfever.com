1cf59af23b1bdddb965bb53e1edfec29977864aa
uforgot <uforgot@designfever.com>
2018-04-19 22:20:00 +0900

    board/board_detail : 타이틀 부분 h1 -> p 태그로 수정 board/board_list : table 에 type-common class 삭제 project_collect : 추가 작업 예정 project_list : 추가 project_detail : 추가 vacation/ : 관련 페이지 추가 vacation/vacation_list : 결제 미리 보기 페이지는 작업 예정

--- markup_template/assets/js/include.js
+++ markup_template/assets/js/include.js
@@ -9,6 +9,7 @@
 $(window).on('load',function(){
     $('#top').load('/include/top.html .top');
     $('#sub-menu-2').load('/include/top.html .sub-menu-2');
+    $('#sub-menu-3').load('/include/top.html .sub-menu-3');
     $('#sub-menu-5').load('/include/top.html .sub-menu-5');
     $('#sub-menu-7').load('/include/top.html .sub-menu-7');
 });
