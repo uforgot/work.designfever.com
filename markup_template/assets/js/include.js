8a7a0c97ea0c817805185df258cf3defda2a2a99
uforgot <uforgot@designfever.com>
2018-04-23 19:07:29 +0900

    org_chart/person_list.html 조직도 추가

--- markup_template/assets/js/include.js
+++ markup_template/assets/js/include.js
@@ -8,8 +8,14 @@
 
 $(window).on('load',function(){
     $('#top').load('/include/top.html .top');
+    $('#sub-menu-1').load('/include/top.html .sub-menu-1');
     $('#sub-menu-2').load('/include/top.html .sub-menu-2');
     $('#sub-menu-3').load('/include/top.html .sub-menu-3');
+    $('#sub-menu-4').load('/include/top.html .sub-menu-4');
     $('#sub-menu-5').load('/include/top.html .sub-menu-5');
+    $('#sub-menu-6').load('/include/top.html .sub-menu-6');
     $('#sub-menu-7').load('/include/top.html .sub-menu-7');
+    $('#sub-menu-8').load('/include/top.html .sub-menu-8');
+    $('#sub-menu-9').load('/include/top.html .sub-menu-9');
+    $('#sub-menu-10').load('/include/top.html .sub-menu-10');
 });
