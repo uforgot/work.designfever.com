527220957804a706b6b304274c63699a55763b0a
hihigh1018 <parkhi@designfever.com>
2018-07-26 18:25:15 +0900

    hihigh : df work background contents - 동영상 컨텐츠 수정

--- dev/bg-contents/common/js/VideoPlayer.js
+++ dev/bg-contents/common/js/VideoPlayer.js
@@ -1,42 +1,87 @@
 var VideoPlayer = function(dataObj){
 
-    var videoData = dataObj.videodata;
-    var videoLowData = dataObj.videodata_low || [];
-    var videoIndex = dataObj.videoindex || parseInt(Math.random() * videoData.length);
-
-    var isMobile = document.querySelector("html").classList.contains("mobile");
+    var video_url = dataObj.video_url;
+    var image_url = dataObj.image_url || "../../common/img/default_bg_image.jpg";
+    var only_video = dataObj.only_video || false;
 
     var _setting = function(){
-
-        _setElement();
-
+        _setElement(only_video ? true : _checkPlayType());
     };
 
-    var _setElement = function(){
+    var _setElement = function(isVideoAble){
+        if(isVideoAble){
+            // play video
+            var video = document.querySelector("video");
+            video.style.opacity = 1;
+            video.src = video_url;
+            video.play();
+        } else {
+            //show image
+            var wrapper = document.querySelector(".video-player .video-wrapper");
+            wrapper.innerHTML = "";
+            wrapper.style.backgroundImage = "url("+image_url+")";
 
-        var defaultData = videoData[videoIndex] || videoData[0];
-        var lowData = videoLowData[videoIndex] || videoData[0];
+        }
+    };
 
-        if(isMobile && lowData) defaultData = lowData;
 
-        var video = document.createElement('video');
-        video.src = defaultData;
-        video.autoplay = true;
-        video.muted = true;
-        video.loop = true;
+    var _checkPlayType = function(){
+        var isVideoAble = false;
 
-        var container = document.querySelector(".video-wrapper");
-        container.prepend(video);
+        var html = document.querySelector("html");
+        var isMobile = html.classList.contains("mobile");
 
+        var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || navigator.msConnection;
+
+        if(isMobile){
+            var isAndroid = html.classList.contains("android");
+            if(isAndroid){
+                // andriod && wifi
+
+                // if (connection && connection.type === 'wifi') {
+                if (connection && connection.type != 'cellular') {
+                    isVideoAble = true;
+                }
+            }
+
+        } else {
+            isVideoAble = true;
+        }
+
+        if(isVideoAble){
+            var supportVideo = supportsVideoType("h264");
+            isVideoAble = supportVideo == "probably" ? isVideoAble : false;
+        }
+        return isVideoAble;
     };
 
 
     var _init = function(){
 
+    };
+
+
+
+    var supportsVideoType = function(type) {
+        let video;
+
+        // Allow user to create shortcuts, i.e. just "webm"
+        let formats = {
+            ogg: 'video/ogg; codecs="theora"',
+            h264: 'video/mp4; codecs="avc1.42E01E"',
+            webm: 'video/webm; codecs="vp8, vorbis"',
+            vp9: 'video/webm; codecs="vp9"',
+            hls: 'application/x-mpegURL; codecs="avc1.42E01E"'
+        };
+
+        if(!video) {
+            video = document.createElement('video')
+        }
+
+        return video.canPlayType(formats[type] || type);
     }
 
     return {
-        setting : _setting,
-        init: _init
+        setting : _setting
     }
 };
