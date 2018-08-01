19db27d48acbd21f124ea40372bd54da1e677d95
Han Youngsu <Poiu0987>
2018-08-01 16:50:23 +0900

    vamstudio : login input css edit

--- dev/assets_login/js/login/loginBgController.js
+++ dev/assets_login/js/login/loginBgController.js
@@ -4,14 +4,22 @@ var loginBgController = function(con_iframe, json_data){
 
     var con_iframe = con_iframe;
 
+    var dim;
+
     function init(){
+
+        if(con_iframe) {
+            dim = con_iframe.querySelector('.dim');
+            console.log("[ loadBgController.js ] : init - dim : ", dim);
+        }
 
         console.log("[ loadBgController.js ] : init - iframe : ", con_iframe);
         console.log("[ loadBgController.js ] : init - json_data : ", json_data);
 
         if(json_data){
 
-            var url = json_data.info.today.bg_contents[0].url;
+            var ran_index = Math.floor(json_data.info.today.bg_contents.length * Math.random());
+            var url = json_data.info.today.bg_contents[ran_index].url;
 
             console.log("[ loadBgController.js ] : ",  "loaded json : ", json_data);
             console.log("[ loadBgController.js ] : ",  "iframe url : ", url);
@@ -28,6 +36,12 @@ var loginBgController = function(con_iframe, json_data){
             ifrm.setAttribute("src", url);
             ifrm.setAttribute("name", "iframe-bg");
             con_iframe.appendChild(ifrm);
+
+            setTimeout(function(){
+                if(dim){
+                    df.lab.Util.addClass(dim, "show");
+                }
+            }, 2000)
         }
     }
 
