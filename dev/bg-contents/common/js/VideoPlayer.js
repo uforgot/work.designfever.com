1aa85067b955d2cc40117374cab3e8b17ebf4cec
hihigh1018 <parkhi@designfever.com>
2018-08-03 13:56:25 +0900

    hihigh : df work background contents video type 모바일 자동재생 수정

--- dev/bg-contents/common/js/VideoPlayer.js
+++ dev/bg-contents/common/js/VideoPlayer.js
@@ -1,11 +1,17 @@
 var VideoPlayer = function(dataObj){
 
     var video_url = dataObj.video_url;
+    var video_low_url = dataObj.video_low_url;
     var image_url = dataObj.image_url || "../../common/img/default_bg_image.jpg";
-    var only_video = dataObj.only_video || false;
+    var only_video = dataObj.only_video == undefined ? true : dataObj.only_video;
+    var dimmedOpacity = dataObj.dimmed_opacity == undefined ? 0.4 : dataObj.dimmed_opacity;
+
+    var _html = document.querySelector("html");
+    var _isMobile = _html.classList.contains("mobile");
 
     var _setting = function(){
-        _setElement(only_video ? true : _checkPlayType());
+        _setElement(!only_video ? _checkPlayType() : true);
+        _makeDimmed();
     };
 
     var _setElement = function(isVideoAble){
@@ -15,7 +21,7 @@ var VideoPlayer = function(dataObj){
             video.style.opacity = 1;
 
             var source = document.querySelector("video source");
-            source.src = video_url;
+            source.src = _isMobile && video_low_url ? video_low_url : video_url;
             // video.src = video_url;
             // video.crossOrigin = 'anonymous';
             video.oncanplay = function() {
@@ -36,13 +42,11 @@ var VideoPlayer = function(dataObj){
     var _checkPlayType = function(){
         var isVideoAble = false;
 
-        var html = document.querySelector("html");
-        var isMobile = html.classList.contains("mobile");
 
         var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection || navigator.msConnection;
 
-        if(isMobile){
-            var isAndroid = html.classList.contains("android");
+        if(_isMobile){
+            var isAndroid = _html.classList.contains("android");
             if(isAndroid){
                 // andriod && wifi
 
@@ -63,6 +67,17 @@ var VideoPlayer = function(dataObj){
         return isVideoAble;
     };
 
+    var _makeDimmed = function(){
+        var alpha = dimmedOpacity;
+        if(alpha == 0) return;
+
+        var dimmed = document.createElement("div");
+        dimmed.classList.add("dimmed");
+        dimmed.style.backgroundColor = "rgba(0,0,0,"+alpha+")";
+
+        var container = document.querySelector(".video-wrapper");
+        container.appendChild(dimmed);
+    };
 
     var _init = function(){
 
